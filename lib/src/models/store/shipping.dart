3ebdcadd40e91237b9244a7fe9944b61fa12fa50
Andrei Cosma <andrei.cosma@teta.so>
2022-11-02 17:53:54 +0200

    feature(Added from schema to shipping)

--- lib/src/models/store/shipping.dart
+++ lib/src/models/store/shipping.dart
@@ -11,6 +11,13 @@ class Shipping {
   final num cost;
   final String description;
 
+  static Shipping fromSchema(final Map<String, dynamic> json) => Shipping(
+        id: json['id'] as String? ?? '',
+        name: json['name'] as String? ?? '',
+        cost: json['cost'] as num? ?? 0,
+        description: json['description'] as String? ?? '',
+      );
+
   Map<String, dynamic> toJson() {
     return <String, dynamic>{
       'id': id,
