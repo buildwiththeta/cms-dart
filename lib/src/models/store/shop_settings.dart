35c627058f63974f3531159fc75750146c632a48
Andrei Cosma <andrei.cosma@teta.so>
2022-11-02 11:25:12 +0200

    feature(added additional fields in shop settings)

--- lib/src/models/store/shop_settings.dart
+++ lib/src/models/store/shop_settings.dart
@@ -1,14 +1,25 @@
 class ShopSettings {
   ShopSettings({
     required this.currency,
+    required this.livemode,
+    required this.merchantCountryCode,
+    required this.merchantDisplayName,
   });
 
   final String currency;
+  final bool livemode;
+  final String merchantCountryCode;
+  final String merchantDisplayName;
 
   Map<String, dynamic> toJson() => <String, dynamic>{
         'currency': currency,
       };
-  static ShopSettings fromSchema(final Map<String, dynamic> json) => ShopSettings(
-    currency: json['currency'],
-  );
+
+  static ShopSettings fromSchema(final Map<String, dynamic> json) =>
+      ShopSettings(
+        currency: json['currency'] as String,
+        livemode: json['livemode'] as bool? ?? false,
+        merchantCountryCode: json['merchantCountryCode'] as String,
+        merchantDisplayName: json['merchantDisplayName'] as String,
+      );
 }
