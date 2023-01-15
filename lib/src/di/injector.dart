f3fd3258bf4f786080cf2c1e60561d63bb8720b0
Andrea Buttarelli on Mac <andreabuttarelli@outlook.com>
2023-01-15 16:25:15 +0100

    Update injector.dart

--- lib/src/di/injector.dart
+++ lib/src/di/injector.dart
@@ -4,7 +4,7 @@ import 'package:injectable/injectable.dart';
 import 'package:teta_cms/src/di/injector.config.dart';
 
 ///Get the Service locator instance
-final getIt = GetIt.instance;
+final getIt = GetIt.I;
 
 /// Flag is initialized
 bool diInitialized = false;
