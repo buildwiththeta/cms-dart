b730d54d5990afc134b8a44ea236d6764690d83c
Andrea Buttarelli <49411143+andreabuttarelli@users.noreply.github.com>
2023-02-11 11:51:03 +0100

    Update provider.dart

--- lib/src/models/provider.dart
+++ lib/src/models/provider.dart
@@ -1,17 +1,13 @@
 /// Supported social OAuth providers
 enum TetaProvider {
-  /// Published
   google,
-
-  /// Published
   github,
-
-  /// Working on
   twitter,
-
-  /// To do
   facebook,
-
-  /// Published
   apple,
+  twitch,
+  linkedin,
+  discord,
+  gitlab,
+  bitbucket,
 }
